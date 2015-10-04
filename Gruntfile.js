5ad9928c3842fbea9c6ad5204439849852171409
pash <pavel.shulin@dibs.com.ua>
2015-10-04 22:08:32 +0300

    add less precompilator task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,15 +10,26 @@ module.exports = function(grunt) {
           transform: ['babelify']
         }
       }
+    },
+    less: {
+      build: {
+        options: {
+          paths: ["src/css"]
+        },
+        files: {
+          "public/style.css": "src/css/style.less"
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   grunt.registerTask('help', function showHelpInfo () {
     console.log(grunt.file.read("README.md"));
   });
 
   grunt.registerTask('default', ['help']);
-  grunt.registerTask('build', ['browserify']);
+  grunt.registerTask('build', ['browserify', 'less:build']);
 };
