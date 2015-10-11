c08897d931a5a0e12fcd7b88a73c8affd7ac40c3
pash <pavel.shulin@dibs.com.ua>
2015-10-12 02:36:19 +0300

    fix png img sources in config

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@ var config = {
 	// arbitrary number of images
 	images: [
 		'public/img/shepard.jpg',
-		'public/img/ida.jpg',
+		'public/img/ida.png',
 		'public/img/garrus.jpg',
 		'public/img/mordin.jpg',
-		'public/img/joker.jpg',
+		'public/img/joker.png',
 		'public/img/liara.jpg'
 	],
 	// possible values: 'auto', 'manual', 'automanual'
