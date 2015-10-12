54085ccbfe8a75416b415b5e3e4e70a0b71085de
pash <pavel.shulin@dibs.com.ua>
2015-10-12 03:25:59 +0300

    add listeners to calculate slide to left or right

--- src/js/slider/index.js
+++ src/js/slider/index.js
@@ -4,12 +4,14 @@ class Slider {
 	constructor (config) {
 		this._items = new Map();
 		this._length = 0;
+		this._userSlideState = {};
 
 		this._initializeStaticProperties();
 		this.config = config || this.defaultConfig;
 
 		this._initializeElements()
-			._createItems();
+			._createItems()
+			.initializeListeners();
 
 		this._setActive(this._getItem(0));
 
@@ -72,18 +74,71 @@ class Slider {
 		return this;
 	}
 
-	_setActive (item) {
-		$.prop(item, {
-			'class': this.ACTIVE_CLASS
+	_getMouseEventX (event) {
+		return event.clientX;
+	}
+
+	_getTouchEventX (event) {
+		var touch = event.touches[0];
+
+		return touch && touch.pageX;
+	}
+
+	_calculateDirection (x1, x2) {
+		return x1 - x2 > 0 && 'left' || 'right';
+	} 
+
+	initializeListeners () {
+		$.on(this._$container, 'mousedown', (event) => {
+			this._onSlideStart(this._getMouseEventX(event));
 		});
+		$.on(this._$container, 'touchstart', (event) => {
+			this._onSlideStart(this._getTouchEventX(event));
+		});
+
+		$.on(this._$container, 'mousemove', (event) => {
+			this._onSlideMove(this._getMouseEventX(event));
+		});
+		$.on(this._$container, 'touchmove', (event) => {
+			this._onSlideMove(this._getTouchEventX(event));
+		});
+
+		$.on(this._$container, 'mouseup', (event) => {
+			this._onSlideEnd(event);
+		});
+		$.on(this._$container, 'touchend', (event) => {
+			this._onSlideEnd(event);
+		});
+	}
+
+	_onSlideStart (x) {
+		this._userSlideState.slide = true;
+		this._userSlideState.start = x; 
+	}
+
+	_onSlideMove (x) {
+		if (this._userSlideState.slide) {
+			this._userSlideState.slide = false;
+			this._direction = this.
+				_calculateDirection(this._userSlideState.start, x);
+		}
+	}
+
+	_onSlideEnd (event) {
+		this._userSlideState.slide = false;
+	}
+
+	_setActive (item) {
+		$.addClass(item, this.ACTIVE_CLASS);
 	}
 
 	_getItem (index) {
 		return this._items.get(index);
 	}
+
 	_setItem (index, item) {
 		this._items.set(index, item);
-	}
+	}	
 }
 
 
